ba2c5583035b2080165846888974652081456fe9
yeshodhan <yeshodhan.kulkarni@tp-link.com>
2016-11-18 17:54:42 -0800

    bug fix

--- batman.rb
+++ batman.rb
@@ -3,7 +3,7 @@ require "formula"
 class Batman < Formula
   homepage "http://www.tp-link.com/us/"
   url "https://s3-us-west-2.amazonaws.com/tpra-app/batman/batman-1.2.68-SNAPSHOT.tar.gz"
-  sha256 "2f030b0b699dda4e9d974086c67c21a1f73eca0aa23355664625a6b7f90ef3c4"
+  sha256 "37f125ff89930142708e5a24322f30064d2232f0df6aba93b1202a50eaef7095"
 
   def install
     inreplace "brew/batman", "##INSTALLDIR##", "#{prefix}"
