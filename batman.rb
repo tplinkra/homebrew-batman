044f1c45b7a98c3b5330803f726bfb8ae677088c
yeshodhan <yeshodhan.kulkarni@tp-link.com>
2016-11-18 17:35:48 -0800

    batman formula bug fix

--- batman.rb
+++ batman.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class Srcclr < Formula
+class batman < Formula
   homepage "http://www.tp-link.com/us/"
   url "https://s3-us-west-2.amazonaws.com/tpra-app/batman/batman-1.2.68-SNAPSHOT.tar.gz"
   sha256 "2f030b0b699dda4e9d974086c67c21a1f73eca0aa23355664625a6b7f90ef3c4"
