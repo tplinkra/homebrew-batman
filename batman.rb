335ed1ffe1d7e5b54ad39bd7204a2c0a28147a4c
yeshodhan <yeshodhan.kulkarni@tp-link.com>
2016-11-19 17:18:52 -0800

    release 0.1.0

--- batman.rb
+++ batman.rb
@@ -3,7 +3,7 @@ require "formula"
 class Batman < Formula
   homepage "http://www.tp-link.com/us/"
   url "https://s3-us-west-2.amazonaws.com/tpra-app/batman/batman-0.1.0.tar.gz"
-  sha256 "c528617f4ee5b7c0dc654005dd0c7419ab3cdfbfb790a43c73dd26a8d4d9337a"
+  sha256 "516dbd87cb28b635b2ae5f774fb83129a9802a2e14750205f0a0db7bb75489ec"
 
   def install
     inreplace "brew/batman", "##INSTALLDIR##", "#{prefix}"
