a513d8fc5ebfa696a215351a2258e0ed5a8e7a5a
Jenkins <jenkins@ip-172-31-47-38.us-west-2.compute.internal>
2017-01-03 19:20:48 +0000

    release 0.1.12

--- batman.rb
+++ batman.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Batman < Formula
   homepage "http://www.tp-link.com/us/"
-  url "https://s3-us-west-2.amazonaws.com/tpra-app/batman/batman-0.1.11.tar.gz"
-  sha256 "72ec7423d11d867563c00c4abd72c74b278f4f51d987dde133d5caa70dfa004d"
+  url "https://s3-us-west-2.amazonaws.com/tpra-app/batman/batman-0.1.12.tar.gz"
+  sha256 "d887662916a5541ac33e37f53cfe81ecc90833b98c8011f61e4708268e6933d4"
 
   def install
     inreplace "brew/bin/batman", "##INSTALLDIR##", "#{prefix}"
