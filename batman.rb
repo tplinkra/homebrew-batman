a5e3ef8c6389eca82f1bab26f49a82ab5d0c6650
Jenkins <jenkins@ip-172-31-47-38.us-west-2.compute.internal>
2017-01-16 20:50:44 +0000

    release 0.1.29

--- batman.rb
+++ batman.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Batman < Formula
   homepage "http://www.tp-link.com/us/"
-  url "https://s3-us-west-2.amazonaws.com/tpra-app/batman/batman-0.1.28.tar.gz"
-  sha256 "c9947348a972be2759b2ef39ebd6e2cfe1a56b713f7c26d0f872ff9a23e36e52"
+  url "https://s3-us-west-2.amazonaws.com/tpra-app/batman/batman-0.1.29.tar.gz"
+  sha256 "01d332b9bc8d41051e13a5dd8c7bb3474854e964fb13f1dc5713e96e2a4790c0"
 
   def install
     inreplace "brew/bin/batman", "##INSTALLDIR##", "#{prefix}"
@@ -12,5 +12,6 @@ class Batman < Formula
     bin.install "brew/bin/batman"
     bin.install "brew/bin/batmanlog"
     bin.install "brew/bin/batmanlog-discovery"
+    bin.install "brew/bin/batmanlog-events"
   end
 end
