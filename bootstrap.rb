cf46ed4784ac2105773a7441abf2e844a65f554c
yeshodhan <yeshodhan.kulkarni@gmail.com>
2017-08-16 16:24:52 -0700

    Bootstrap Release 0.0.6

--- bootstrap.rb
+++ bootstrap.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Bootstrap < Formula
   homepage "http://www.tp-link.com/us/"
-  url "https://s3-us-west-2.amazonaws.com/tpra-app/bootstrap/bootstrap-0.0.5.tar.gz"
-  sha256 "1038b3e5ca980c45e08c58e1ab0aeb6be98d632848a51ec6205d736062c76132"
+  url "https://s3-us-west-2.amazonaws.com/tpra-app/bootstrap/bootstrap-0.0.6.tar.gz"
+  sha256 "2d2911f099bdc3638630b691c2be7ef6a9321e5bc8a834591b76adb817a6df3d"
 
   def install
     bin.install "brew/bin/dev"
