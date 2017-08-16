217af4f1464973c26f92091d4d82cce807cc29a4
Jenkins <jenkins@ip-172-31-47-38.us-west-2.compute.internal>
2017-08-16 20:07:01 +0000

    release 0.0.3

--- bootstrap.rb
+++ bootstrap.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Bootstrap < Formula
   homepage "http://www.tp-link.com/us/"
-  url "https://s3-us-west-2.amazonaws.com/tpra-app/bootstrap/bootstrap-0.0.2.tar.gz"
-  sha256 "d30fad777c85e8e3162165942ab2dd78cbc70cc35791b952dce15d99a10c5dfc"
+  url "{{DISTRIBUTION_URL}}"
+  sha256 "{{CHECKSUM}}"
 
   def install
     bin.install "brew/bin/dev"
