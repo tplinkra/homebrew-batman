537ed6cc5355abfc03261a8dd376e19e60107009
Jenkins <devops@tplinkra.com>
2017-09-17 16:16:30 +0000

    Bootstrap Release 0.0.29

--- bootstrap.rb
+++ bootstrap.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Bootstrap < Formula
   homepage "http://www.tp-link.com/us/"
-  url "https://s3-us-west-2.amazonaws.com/tpra-app/bootstrap/bootstrap-0.0.28.tar.gz"
-  sha256 "f29bf4104b4fd1cde416aa1b3a5d104cdde072960c0731efcf5783df709257d0"
+  url "https://s3-us-west-2.amazonaws.com/tpra-app/bootstrap/bootstrap-0.0.29.tar.gz"
+  sha256 "acd62f048cd496b7f41aa4e106ac7ee902481c135a415dd02c14be01e9273f42"
 
   def install
     bin.install "brew/bin/robin"
