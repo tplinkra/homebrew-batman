fff5687e1e237a8500a84cbf63dfe729c6f33589
Jenkins <devops@tplinkra.com>
2017-10-18 20:17:44 +0000

    release 0.1.75

--- batman.rb
+++ batman.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Batman < Formula
   homepage "http://www.tp-link.com/us/"
-  url "https://s3-us-west-2.amazonaws.com/tpra-app/batman/batman-0.1.74.tar.gz"
-  sha256 "c5e5e70d078a56b5fe798c7f152c7f838cad42d19d05cc0f4edb8847748d90f4"
+  url "https://s3-us-west-2.amazonaws.com/tpra-app/batman/batman-0.1.75.tar.gz"
+  sha256 "0dd4addb374e7a5fd721d93d1373d9f1849116193c74f6be75de20ae497fcf98"
 
   def install
     inreplace "brew/bin/batman", "##INSTALLDIR##", "#{prefix}"
