35495452a4ec9e6f8eac98e4bc7490a85b3d8419
Jenkins <devops@tplinkra.com>
2019-05-25 08:50:00 +0000

    Hawkfire Release: v1.0.2

--- hawkfire.rb
+++ hawkfire.rb
@@ -3,22 +3,21 @@ require "formula"
 class Hawkfire < Formula
   desc "Hawkfire - Superhero Utility for Kubernetes Project"
   homepage "https://www.kasasmart.com/"
-  url "https://s3-us-west-2.amazonaws.com/tpra-app/hawkfire/hawkfire-1.0.1.tar.gz"
-  sha256 "a7bcd6a066b205767c5fdc31d8d7ca7d5fbc35c82648477203f59db8f5c286ba"
+  url "https://s3-us-west-2.amazonaws.com/tpra-app/hawkfire/hawkfire-1.0.2.tar.gz"
+  sha256 "e85bbcdf149372b9c22d3f20746426a8062a755445d22ff6ad202a3634891403"
 
   depends_on "node"
   depends_on "yarn"
 
   def install
-    prefix.install "brew/lib"
-    prefix.install "brew/README.md"
-    prefix.install "brew/yarn.lock"
-    prefix.install "brew/package.json"
+    system "yarn", "install"
 
-    bin.install "brew/bin/hawkfire"
-    bin.install "brew/bin/hawkfire-init"
-    bin.install "brew/bin/hawkfire-list"
+    prefix.install "lib"
+    prefix.install "README.md"
+    prefix.install "yarn.lock"
+    prefix.install "package.json"
+    prefix.install "node_modules"
 
-    system "yarn" "install"
+    bin.install Dir["bin/*"]
   end
 end
