a5832712ecff36fd55120a4bba59cd5c7670eba1
Jenkins <devops@tplinkra.com>
2019-05-28 01:22:40 +0000

    Hawkfire Release: v1.0.6

--- hawkfire.rb
+++ hawkfire.rb
@@ -3,8 +3,8 @@ require "formula"
 class Hawkfire < Formula
   desc "Hawkfire - Superhero Utility for Kubernetes Project"
   homepage "https://www.kasasmart.com/"
-  url "https://s3-us-west-2.amazonaws.com/tpra-app/hawkfire/hawkfire-1.0.5.tar.gz"
-  sha256 "f2a01924e171e886892cfa3b653d564fc7ac8986ba6326ae5b90da2a6d6624e2"
+  url "https://s3-us-west-2.amazonaws.com/tpra-app/hawkfire/hawkfire-1.0.6.tar.gz"
+  sha256 "9fe4593e81d8e77465a7768ee2203fdacec0e79a62e3eb36c6aa73ac7a52c5fd"
 
   depends_on "node"
   depends_on "yarn"
